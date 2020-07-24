a828aa6e77d32d701682cf26dbb647b5dac866e9
Zatarita <51307516+Zatarita@users.noreply.github.com>
2020-07-24 06:13:42 -0400

    Update fmeta.py

--- Saber/fmeta.py
+++ Saber/fmeta.py
@@ -8,28 +8,28 @@ class fmeta():
 
     class item():
         string = ""
-        type = -1
+        type = 0
         decompressed_size = -1
 
         def __init__(self, stream = None):
-            if stream = None: return
+            if not stream: return
             self.load_from_stream(stream)
 
         def load_from_stream(self, stream):
             self.string = stream.read(0x100).decode("utf-8").rstrip("\0")
-            stream.read(8) #burn padding
+            stream.read(4) #burn padding
             self.type = int.from_bytes(stream.read(4), "little")
             self.decompressed_size = int.from_bytes(stream.read(4), "little")
+            stream.read(4) #burn padding
 
         def generate_from_file(self, string, type, size):
-            # TO-DO: the string is local to the halo directory
 
-            self.string = "/Local/Halo/Directory/" + string
+            self.string = string
             self.type = type
             self.size = size
 
     def __init__(self, data = None):
-        if data = None: return
+        if not data: return
         self.load(data)
 
     def load(self, data):
@@ -38,7 +38,7 @@ class fmeta():
         stream.read(4) #burn padding
 
         for i in range(count):
-            items.append(self.item(stream))
+            self.items.append(self.item(stream))
 
     def compile_data(self):
         output = io.BytesIO()
@@ -46,23 +46,34 @@ class fmeta():
         output.write(b"\0" * 4)
         for item in self.items:
             output.write(item.string.ljust(0x100, "\0").encode("utf-8"))
-            output.write('\0' * 8)
+            output.write(b'\0' * 4)
             output.write(pack("<i", item.type))
-            output.write(pack("<i", item.decompressed_size))
+            if item.type == 0:
+                output.write(b'\xff' * 8)
+            else:
+                output.write(pack("<i", item.decompressed_size))
+                output.write(b'\00' * 4)
 
-        #TODO Update the 18kb with actual file size
-        #output.write('\0' * 18kb - output.tell())
+        output.write(b'\0' * (0x4408 - output.tell()))
 
         return output.getvalue()
 
     def add_entry(self, file, size):
+        if len(self.items) == 40:
+            print("Exceeds maximum file size. Ignoring entry.")
+            return
         new_item = self.item()
-        if size == -1:
-            new_item.generate_from_file(file, -1 ,size)
+        if size == 0:
+            new_item.generate_from_file(file, 0 ,size)
         else:
             new_item.generate_from_file(file, 1, size)
 
         self.items.append(new_item)
+
+    def delete(self, string):
+        for item in self.items:
+            if item.string == string:
+                self.items.remove(item)
 
     def get_decompressed_size(path):
         with open(path, "rb") as file:
