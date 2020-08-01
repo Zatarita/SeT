524878a8d541143081f98858d6981bf62e075898
Zatarita <51307516+Zatarita@users.noreply.github.com>
2020-08-01 13:28:15 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Zatarita",
+    name="SeT",
     version="0.0.1",
     author="Zatarita",
     author_email="richarda.mooreiii@gmail.com",
