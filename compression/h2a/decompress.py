3969d648910491c0aadb8c28d8c8b1e3ccb4077c
Zatarita <51307516+Zatarita@users.noreply.github.com>
2020-10-05 13:10:34 -0400

    h2a decompression

---
+++ compression/h2a/decompress.py
@@ -0,0 +1,20 @@
+import zlib, os
+from common.data_parsing import StreamParser
+
+
+def decompress(data):
+    stream = StreamParser(data)
+    count = stream.readInt(8)
+
+    offsets = [stream.readInt(8) for i in range(count)]
+    offsets.append(len(data))
+
+    with open("tmp", "wb") as file:
+        for i in range(len(offsets) - 1):
+            file.write(zlib.decompress(data[offsets[i] : offsets[i + 1]]))
+
+    decompressed_data = open("tmp", "rb").read()
+
+    os.remove("tmp")
+
+    return decompressed_data
