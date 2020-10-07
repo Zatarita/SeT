901f229d871ae82a2012639c4238e619a1c1ab9d
Zatarita <51307516+Zatarita@users.noreply.github.com>
2020-10-07 11:39:49 -0400

    Delete scratch.py

--- scratch.py
+++
@@ -1,9 +0,0 @@
-from saber.file.ipak import Ipak
-from os import listdir
-import cProfile
-
-
-ipak = Ipak("D:/SteamLibrary/steamapps/common/Halo The Master Chief Collection/halo1/prebuild/paks/inplace1.ipak")
-
-cProfile.runctx("ipak.save('inplace1.ipak_decompressed')", globals(), locals())
-
